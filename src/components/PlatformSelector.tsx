36ec6f4b692f54d453521440bdd5b0da28fef1d1
smdmori <mortezasamadi1371@gmail.com>
2023-08-13 19:50:17 +0330

    Fetching Platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -15,7 +15,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>{selectedPlatform?.name || 'Platforms'}</MenuButton>
       <MenuList>
-        {data.map(platform => <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)}>{platform.name}</MenuItem>)}
+        {data?.results.map(platform => <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)}>{platform.name}</MenuItem>)}
       </MenuList>
     </Menu>
   )
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,6 +1,13 @@
-import useData from "./useData";
+import {useQuery} from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import {FetchData} from "./useData";
 import {Platform} from "./useGames";
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () => apiClient.get<FetchData<Platform>>("/platforms/lists/parents").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+  });
 
 export default usePlatform;
